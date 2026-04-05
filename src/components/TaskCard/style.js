d3dda773d8cd6a12963901f599ddb27fde56de73
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 13:53:58 -0300

    feat: Add TaskCard style

--- src/components/TaskCard/style.js
+++ src/components/TaskCard/style.js
@@ -4,19 +4,39 @@ import { theme } from "../../styles/global";
 export const styles = StyleSheet.create({
 
 	card: {
-
+		backgroundColor: "#FFFFFF",
+		padding: 15,
+		borderRadius: 15,
+		marginBottom: 10,
+		flexDirection: "row",
+		borderLeftWidth: 6,
+		elevation: 2
 	},
 	title: {
-
+		fontSize: 17,
+		fontWeight: "bold",
+		color: theme.colors.textMain
 	},
 	description: {
-
+		fontSize: 14,
+		fontWeight: "bold",
+		color: theme.colors.textSub
 	},
 	badge: {
-
+		fontSize: 11,
+		fontWeight: "bold",
+		marginTop: 5
+	},
+	date: {
+		fontSize: 10,
+		color: "#94A3B8",
+		marginTop: 5
 	},
 	action: {
-
+		flexDirection: "row",
+		gap: 15,
+		alignItems: "center",
+		marginLeft: 10
 	},
 	icons: {
 
