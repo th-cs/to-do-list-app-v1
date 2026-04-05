c62d8a750d07a1ed50f382357f02645b918184d9
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 07:06:13 -0300

    feat: Add TaskCard component

---
+++ src/components/TaskCard/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../styles/global";
+
+export const styles = StyleSheet.create({
+
+	card: {
+
+	},
+	title: {
+
+	},
+	description: {
+
+	},
+	badge: {
+
+	},
+	action: {
+
+	},
+	icons: {
+
+	}
+
+});
