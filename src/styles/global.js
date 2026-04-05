bf161acf025030939878ea59b7d2ce5b41b7deea
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 13:56:20 -0300

    style: Change quotation marks

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,15 @@
 export const theme = {
 	colors: {
-		background: '#F1F5F9',
-		primary: '#4F46E5',
-		danger: '#EF4444',
-		textMain: '#1E293B',
-		textSub: '#64748B',
-		border: '#CBD5E1',
+		background: "#F1F5F9",
+		primary: "#4F46E5",
+		danger: "#EF4444",
+		textMain: "#1E293B",
+		textSub: "#64748B",
+		border: "#CBD5E1",
 		priority: {
-			High: '#EF4444',
-			Medium: '#F59E0B',
-			Low: '#10B981',
+			High: "#EF4444",
+			Medium: "#F59E0B",
+			Low: "#10B981",
 		}
 	}
 }
