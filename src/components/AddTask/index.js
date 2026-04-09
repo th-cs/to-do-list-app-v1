d66ac206e600de315e9e6086370008808c683add
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-09 11:15:44 -0300

    feat: Add AddTask component

---
+++ src/components/AddTask/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Modal, View, Text, TextInput, TouchableOpacity } from "react-native";
+import styles from "./style.js";
+
+export default function AddTask({ visible, onSave, onClose }){
+	const [task, setTask] = useState({
+		title: "",
+		description: "",
+		priority: "",
+		startDate: "",
+		endDate: ""
+	});
+
+	const handleSave = () => {
+		onSave(task);
+		setTask({
+			title: "",
+			description: "",
+			priority: "",
+			startDate: "",
+			endDate: ""
+		});
+	}
+
+	return(
+		<Modal
+			visible={visible}
+			animationType="slide">
+			<View style={styles.modal}>
+				<Text style={styles.title}>
+					Nova tarefa
+				</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Título da tarefa"
+					onChangeText={t => setTask({
+						...task, title: t
+					})} />
+				<TextInput
+					style={styles.input}
+					placeholder="Descrição da tarefa"
+					onChangeText={t => setTask({
+						...task, description: t
+					})} />
+				<TextInput
+					style={styles.input}
+					placeholder="Data de início: DD/MM"
+					onChangeText={t => setTask({
+						...task, startDate: t
+					})} />
+				<TextInput
+					style={styles.input}
+					placeholder="Data de conclusão: DD/MM"
+					onChangeText={t => setTask({
+						...task, endDate: t
+					})} />
+				<TextInput
+					style={styles.priority}
+					placeholder="Prioridade: Alta/Média/Baixa"
+					onChangeText={t => setTask({
+						...task, priority: t
+					})} />
+
+				<TouchableOpacity
+					style={styles.btnSave}
+					onPress={handleSave}>
+					<Text>
+						Salvar tarefa
+					</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					onPress={onClose}>
+					<Text style={styles.txtCancel}>
+						Cancelar
+					</Text>
+				</TouchableOpacity>
+0
+			</View>
+		</Modal>
+	);
+}
---
+++ src/components/AddTask/style.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../styles/global";
+
+export const styles = StyleSheet.create({
+
+	modal: {
+		flex: 1,
+		padding: 30,
+		backgroundColor: theme.colors.background
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 20
+	},
+	input: {
+		backgroundColor: "#FFFFFF",
+		padding: 15,
+		borderRadius: 8,
+		marginBottom: 10,
+		borderWidth: 1,
+		borderColor: "#DDDDDD"
+	},
+	btnSave: {
+		backgroundColor: theme.colors.primary,
+		padding: 15,
+		borderRadius: 10,
+		alignItems: "center"
+	},
+	txtCancel: {
+		color: "#F00000",
+		textAlign: "center"
+	}
+});
