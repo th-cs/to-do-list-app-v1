5207bc374ce89a355da9ebad52619a8c76453b8e
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-16 11:03:10 -0300

    fix: Add file extension

--- src/components/AddTask/style.js
+++ src/components/AddTask/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { theme } from "../../styles/global";
+import { theme } from "../../styles/global.js";
 
 export const styles = StyleSheet.create({
 
