75e7535852918bb9678b9a69b67e6c9c93fe9e1b
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-16 10:59:11 -0300

    feat: Add App

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { theme } from "./src/styles/global";
+import AddTask from "./src/components/AddTask";
+import EditTask from "./src/components/EditTask";
+import SearchBar from "./src/components/SearchBar";
+import TaskCard from "./src/components/TaskCard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [task, setTask] = useState([]);
+	const [searchText, setSearchText] = useState("");
+	const [addVisible, setAddVisibile] = useState(false);
+	const [editVisible, setEditVisible] = useState(false);
+	const [selectedTask, setSelectedTask] = useState(null);
+
+	const addTask = (newTask) => {
+		setTask([...task, {newTask, id: Date.now().toString() }]);
+		setAddVisibile(false);
+	}
+
+	const updateTask = (updatedTask) => {
+		setTask(task.map(
+			t => t.id === updatedTask.id ? updatedTask: t));
+		setAddVisibile(false);
+	}
+
+	const deleteTask = (id) => setTask(task.filter(t => t.id !== id));
+
+
+	const filterTask = task.filter(
+		t => t.title.toLowerCase().includes(searchText.toLowerCase()));
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.header}>ToDo List</Text>
+			<SearchBar
+				value={searchText}
+				onChangeText={setSearchText} />
+			<FlatList
+				data={filterTask}
+				keyExtractor={item => item.id}
+				renderItem={({ item }) => (
+					<TaskCard
+						item={ item }
+						onDelete={deleteTask}
+						onEdit={openEdit} />
+				)} />
+
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => setAddVisibile(true)}>
+				<Text style={styles.add}>+</Text>
+			</TouchableOpacity>
+			<AddTask
+				visible={addVisible}
+				onSave={addTask}
+				onClose={() => setAddVisibile(false)} />
+			<EditTask
+				visible={editVisible}
+				taskData={selectedTask}
+				onSave={updateTask}
+				onClose={() => setEditVisible(false)} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: theme.colors.background,
+		padding: 20,
+		paddingTop: 60
+	},
+	header: {
+		fontSize: 28,
+		fontWeight: "bold",
+		marginBottom: 20
+	},
+	button: {
+		position: "absolute",
+		bottom: 30,
+		right: 30,
+		backgroundColor: theme.colors.background,
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		justifyContent: "center",
+		alignItems: "center"
+	}
 });
